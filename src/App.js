2400688599bf5166730789f37fbf6ff070269c69
Ritik Raj <raj769417@gmail.com>
2024-02-06 14:02:16 +0530

    download resume

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import gsap from "gsap";
 
 import { useRef, useState, useLayoutEffect } from "react";
 
+const BACKEND = "http://127.0.0.1:3001";
+
 function NavBar() {
     const { scrollY } = useScroll();
     const [hidden, setHidden] = useState(false);
@@ -210,6 +212,12 @@ function HeroSection() {
         return () => ctx.revert();
     }, []);
 
+    async function downloadResume() {
+        const a = document.createElement('a');
+        a.setAttribute("href", `${BACKEND}/resume`);
+        a.click();
+        a.remove();
+    }
 
     return (
         <section className="hero" ref={comp}>
@@ -231,7 +239,10 @@ function HeroSection() {
                             🍵tea.
                         </p>
                         <div className="hero_bottom">
-                            <button className="btn secondary">
+                            <button
+                                className="btn secondary"
+                                onClick={downloadResume}
+                            >
                                 Download CV
                             </button>
                             {/* <button className="btn secondary">
@@ -468,28 +479,44 @@ function Socials() {
     return (
         <section className="socials">
             <div className="container">
-            <h2><span className="highlight2">Have a project in mind ? </span> Let's talk.</h2>
+                <h2>
+                    <span className="highlight2">
+                        Have a project in mind ?{" "}
+                    </span>{" "}
+                    Let's talk.
+                </h2>
                 {/* <img src="/icons/t1.svg" className="test" alt="" /> */}
                 <img src="/icons/t2.svg" className="svg_float" alt="" />
                 <img src="/icons/t3.svg" className="svg_float" alt="" />
                 <div className="social_container">
                     <div>
-                        <img className="social_icon" src="/icons/linkedin.png" alt=""/>
+                        <img
+                            className="social_icon"
+                            src="/icons/linkedin.png"
+                            alt=""
+                        />
                     </div>
                     <div>
-                        <img className="social_icon" src="/icons/gmail.png" alt=""/>
+                        <img
+                            className="social_icon"
+                            src="/icons/gmail.png"
+                            alt=""
+                        />
                     </div>
                     <div>
-                        <img className="social_icon" src="/icons/github.svg" alt=""/>
+                        <img
+                            className="social_icon"
+                            src="/icons/github.svg"
+                            alt=""
+                        />
                     </div>
                 </div>
             </div>
         </section>
-    )
+    );
 }
 
 function App() {
-    
     return (
         <>
             <NavBar />
