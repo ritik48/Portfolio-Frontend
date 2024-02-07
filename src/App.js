5ac41f5f593d65279641550b74f0df4a9e7eb342
Ritik Raj <raj769417@gmail.com>
2024-02-07 22:04:46 +0530

    created navbar component file

--- src/App.js
+++ src/App.js
@@ -1,199 +1,13 @@
-import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 
 import gsap from "gsap";
 
 import { useRef, useState, useLayoutEffect, useEffect } from "react";
 
-const BACKEND = "http://192.168.1.10:3001";
+import NavBar from "./components/NavBar";
 
-function NavBar() {
-    const { scrollY } = useScroll();
-    const [hidden, setHidden] = useState(false);
+const BACKEND = "http://192.168.1.7:3001";
+// const BACKEND = "http://127.0.0.1:3001";
 
-    const [showLogo, setShowLogo] = useState(true);
-
-    useMotionValueEvent(scrollY, "change", (latest) => {
-        const previous = scrollY.getPrevious();
-
-        if (latest > 20) {
-            setShowLogo(false);
-        } else {
-            setShowLogo(true);
-        }
-
-        if (latest > previous && latest > 20) {
-            setHidden(true);
-        } else {
-            setHidden(false);
-        }
-    });
-
-    return (
-        <>
-            <svg
-                className="svg1"
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                // xmlnsXlink="http://www.w3.org/1999/xlink"
-                // xmlnsSvgjs="http://svgjs.dev/svgjs"
-                viewBox="0 0 1422 800"
-            >
-                <defs>
-                    <linearGradient
-                        x1="50%"
-                        y1="0%"
-                        x2="50%"
-                        y2="100%"
-                        id="oooscillate-grad"
-                    >
-                        <stop
-                            stop-color="hsl(206, 75%, 49%)"
-                            stop-opacity="1"
-                            offset="0%"
-                        ></stop>
-                        <stop
-                            stop-color="hsl(331, 90%, 56%)"
-                            stop-opacity="1"
-                            offset="100%"
-                        ></stop>
-                    </linearGradient>
-                </defs>
-                <g
-                    stroke-width="2"
-                    stroke="url(#oooscillate-grad)"
-                    fill="none"
-                    stroke-linecap="round"
-                >
-                    <path
-                        d="M 0 572 Q 355.5 -100 711 400 Q 1066.5 900 1422 572"
-                        opacity="1.00"
-                    ></path>
-                    <path
-                        d="M 0 550 Q 355.5 -100 711 400 Q 1066.5 900 1422 550"
-                        opacity="0.96"
-                    ></path>
-                    <path
-                        d="M 0 528 Q 355.5 -100 711 400 Q 1066.5 900 1422 528"
-                        opacity="0.92"
-                    ></path>
-                    <path
-                        d="M 0 506 Q 355.5 -100 711 400 Q 1066.5 900 1422 506"
-                        opacity="0.89"
-                    ></path>
-                    <path
-                        d="M 0 484 Q 355.5 -100 711 400 Q 1066.5 900 1422 484"
-                        opacity="0.85"
-                    ></path>
-                    <path
-                        d="M 0 462 Q 355.5 -100 711 400 Q 1066.5 900 1422 462"
-                        opacity="0.81"
-                    ></path>
-                    <path
-                        d="M 0 440 Q 355.5 -100 711 400 Q 1066.5 900 1422 440"
-                        opacity="0.77"
-                    ></path>
-                    <path
-                        d="M 0 418 Q 355.5 -100 711 400 Q 1066.5 900 1422 418"
-                        opacity="0.73"
-                    ></path>
-                    <path
-                        d="M 0 396 Q 355.5 -100 711 400 Q 1066.5 900 1422 396"
-                        opacity="0.70"
-                    ></path>
-                    <path
-                        d="M 0 374 Q 355.5 -100 711 400 Q 1066.5 900 1422 374"
-                        opacity="0.66"
-                    ></path>
-                    <path
-                        d="M 0 352 Q 355.5 -100 711 400 Q 1066.5 900 1422 352"
-                        opacity="0.62"
-                    ></path>
-                    <path
-                        d="M 0 330 Q 355.5 -100 711 400 Q 1066.5 900 1422 330"
-                        opacity="0.58"
-                    ></path>
-                    <path
-                        d="M 0 308 Q 355.5 -100 711 400 Q 1066.5 900 1422 308"
-                        opacity="0.54"
-                    ></path>
-                    <path
-                        d="M 0 286 Q 355.5 -100 711 400 Q 1066.5 900 1422 286"
-                        opacity="0.51"
-                    ></path>
-                    <path
-                        d="M 0 264 Q 355.5 -100 711 400 Q 1066.5 900 1422 264"
-                        opacity="0.47"
-                    ></path>
-                    <path
-                        d="M 0 242 Q 355.5 -100 711 400 Q 1066.5 900 1422 242"
-                        opacity="0.43"
-                    ></path>
-                    <path
-                        d="M 0 220 Q 355.5 -100 711 400 Q 1066.5 900 1422 220"
-                        opacity="0.39"
-                    ></path>
-                    <path
-                        d="M 0 198 Q 355.5 -100 711 400 Q 1066.5 900 1422 198"
-                        opacity="0.35"
-                    ></path>
-                    <path
-                        d="M 0 176 Q 355.5 -100 711 400 Q 1066.5 900 1422 176"
-                        opacity="0.32"
-                    ></path>
-                    <path
-                        d="M 0 154 Q 355.5 -100 711 400 Q 1066.5 900 1422 154"
-                        opacity="0.28"
-                    ></path>
-                    <path
-                        d="M 0 132 Q 355.5 -100 711 400 Q 1066.5 900 1422 132"
-                        opacity="0.24"
-                    ></path>
-                    <path
-                        d="M 0 110 Q 355.5 -100 711 400 Q 1066.5 900 1422 110"
-                        opacity="0.20"
-                    ></path>
-                    <path
-                        d="M 0 88 Q 355.5 -100 711 400 Q 1066.5 900 1422 88"
-                        opacity="0.16"
-                    ></path>
-                    <path
-                        d="M 0 66 Q 355.5 -100 711 400 Q 1066.5 900 1422 66"
-                        opacity="0.13"
-                    ></path>
-                    <path
-                        d="M 0 44 Q 355.5 -100 711 400 Q 1066.5 900 1422 44"
-                        opacity="0.09"
-                    ></path>
-                </g>
-            </svg>
-            <motion.nav
-                initial="initial"
-                variants={{
-                    initial: {
-                        x: "-50%",
-                    },
-                    visible: { y: 0 },
-                    hidden: { y: "-140%" },
-                }}
-                animate={hidden ? "hidden" : "visible"}
-                transition={{ duration: 0.35, ease: "easeInOut" }}
-            >
-                <div className="container">
-                    {showLogo && (
-                        <div className="logo">
-                            {"< "} R {"/>"}
-                        </div>
-                    )}
-                    <ul>
-                        <li className="active">Projects</li>
-                        <li>Blogs</li>
-                        <li>Contact</li>
-                    </ul>
-                </div>
-            </motion.nav>
-        </>
-    );
-}
 
 function HeroSection() {
     const comp = useRef(null);
@@ -349,20 +163,21 @@ function Projects() {
     const [projects, setProjects] = useState([]);
 
     useEffect(() => {
-        setError(false);
+        setError("");
         setLoading(true);
         async function fetchProjects() {
             try {
-                const res = await fetch(`${BACKEND}`, {
+                const res = await fetch(`${BACKEND}/projects`, {
                     method: "GET",
                 });
 
                 if (!res.ok) {
-                    return alert("Error fetching projects");
+                    return setError("Unable to fetch projects");
                 }
                 const data = await res.json();
 
                 setProjects(data);
+                setLoading(false);
             } catch (error) {
                 setError("Unable to fetch projects");
             } finally {
@@ -423,17 +238,10 @@ function Projects() {
                                 </div>
                             );
                         })}
-
-                        {/* <div className="project">
-                            <div className="project_img"></div>
-                            <div className="project_title"></div>
-                        </div>
-                        <div className="project">
-                            <div className="project_img"></div>
-                            <div className="project_title"></div>
-                        </div> */}
                     </div>
-                    <button className="btn primary">All Projects</button>
+                    {!error && !loading && projects && (
+                        <button className="btn primary">All Projects</button>
+                    )}
                 </div>
             </div>
         </section>
@@ -441,38 +249,124 @@ function Projects() {
 }
 
 function Blogs() {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    const [blogs, setBlogs] = useState(null);
+
+    useEffect(() => {
+        setError("");
+        setLoading(true);
+        async function fetchBlogs() {
+            try {
+                const res = await fetch(`${BACKEND}/blogs`, {
+                    method: "GET",
+                });
+
+                if (!res.ok) {
+                    return setError("Unable to fetch blogs");
+                }
+                const data = await res.json();
+                console.log(data.tags);
+                setBlogs(data);
+                setLoading(false);
+            } catch (error) {
+                setError("Unable to fetch blogs");
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchBlogs();
+    }, []);
+
+    function getFormattedDate(date) {
+        // Assuming your JSON data is loaded into a variable called jsonData
+        const createdAt = date;
+
+        // Parse the ISO 8601 formatted date string
+        const parsedDate = new Date(createdAt);
+
+        // Function to get the suffix for the day (e.g., 'st', 'nd', 'rd', 'th')
+        function getDaySuffix(day) {
+            if (day >= 11 && day <= 13) {
+                return "th";
+            }
+            switch (day % 10) {
+                case 1:
+                    return "st";
+                case 2:
+                    return "nd";
+                case 3:
+                    return "rd";
+                default:
+                    return "th";
+            }
+        }
+
+        // Format the date as "14th Jan, 2024"
+        const formattedDate = `${parsedDate.getDate()}${getDaySuffix(
+            parsedDate.getDate()
+        )} ${parsedDate.toLocaleString("default", {
+            month: "short",
+        })}, ${parsedDate.getFullYear()}`;
+
+        return formattedDate;
+    }
+
+    const publishedDate = blogs ? getFormattedDate(blogs.createdAt) : "";
+
     return (
         <section className="blogs">
             <div className="container">
                 <h2>Latest Blog</h2>
 
                 <div className="blogs_content">
-                    <div className="blog_left">
-                        <h3 className="blog_title">
-                            Controlled Elements in React
-                        </h3>
-                        <span className="blog_date">25th December, 2023</span>
-                        <div className="blog_tags">
-                            <span>react</span>
-                            <span>useState</span>
-                            <span>hooks</span>
-                        </div>
-                        <div className="blog_read">
-                            <button className="btn secondary">
-                                Read{" "}
-                                <img
-                                    src="/images/link.svg"
-                                    alt=""
-                                    className="icon"
-                                />
-                            </button>
-                        </div>
-                    </div>
-                    <div className="blog_right">
-                        <img src="/images/blog.webp" alt="" />
-                    </div>
+                    {error && <p className="info error">{error}</p>}
+                    {loading && <p className="info">Fetching Blogs...</p>}
+                    {!loading && !error && !blogs && (
+                        <p className="info">Currently no blogs.</p>
+                    )}
+
+                    {!loading && !error && blogs && (
+                        <>
+                            <div className="blog_left">
+                                <h3 className="blog_title">{blogs.title}</h3>
+                                <span className="blog_date">
+                                    {publishedDate}
+                                </span>
+                                <div className="blog_tags">
+                                    {blogs.tags &&
+                                        blogs.tags.map((tag, index) => (
+                                            <span key={index}>{tag}</span>
+                                        ))}
+                                </div>
+                                <div className="blog_read">
+                                    <a
+                                        href={blogs.link}
+                                        className="btn secondary"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Read{" "}
+                                        <img
+                                            src="/images/link.svg"
+                                            alt=""
+                                            className="icon"
+                                        />
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="blog_right">
+                                <img src={blogs.image} alt="" />
+                            </div>
+                        </>
+                    )}
                 </div>
-                <button className="btn primary more_blogs">More Blogs</button>
+                {!error && !loading && blogs && (
+                    <button className="btn primary more_blogs">
+                        More Blogs
+                    </button>
+                )}
             </div>
         </section>
     );
