820e2656b9a1c7d9ba58a3d8da2276f1ab5e69da
Ritik Raj <raj769417@gmail.com>
2024-02-09 02:25:59 +0530

    add function to seacrh project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -14,6 +14,9 @@ function ProjectList() {
 
     const [projects, setProjects] = useState([]);
 
+    const [search, setSearch] = useState("");
+    const [searchProject, setSearchProject] = useState([]);
+
     useEffect(() => {
         setError("");
         setLoading(true);
@@ -38,6 +41,54 @@ function ProjectList() {
         }
         fetchProjects();
     }, []);
+
+    useEffect(() => {
+        const controller = new AbortController();
+        async function fetchProjects() {
+            setError("");
+            setLoading(true);
+
+            try {
+                const res = await fetch(
+                    `${BACKEND}/projects/search?q=${search}`,
+                    {
+                        signal: controller.signal,
+                    }
+                );
+
+                if (!res.ok) {
+                    return setError("Something went wrong. Try again");
+                }
+
+                const data = await res.json();
+                console.log(search, " = ", data);
+                // console.log("data = ", data)
+                setSearchProject(data);
+
+                setError("");
+            } catch (error) {
+                if (error.name !== "AbortError") {
+                    setError(error.message);
+                }
+                
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        if (search.length < 2) {
+            setSearchProject([]);
+            return;
+        }
+
+        setSearchProject([]);
+        fetchProjects();
+
+        return function () {
+            controller.abort();
+        };
+    }, [search]);
+
     return (
         <section className={styles.projects_section}>
             <div className="container">
@@ -46,7 +97,11 @@ function ProjectList() {
                         <h2>My Projects</h2>
                     </div>
                     <div className={styles.project_controls}>
-                        <input placeholder="Seacrh" />
+                        <input
+                            placeholder="Seacrh"
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
                         <div className={styles.project_tags}>
                             <span>Python</span>
                             <span>React</span>
@@ -58,7 +113,7 @@ function ProjectList() {
                         </div>
                     </div>
                     <div className={styles.project_list}>
-                        {error && <p className="info error">{error}</p>}
+                        {error && <p className="info error">{error}4564...</p>}
                         {loading && (
                             <p className="info">Fetching projects...</p>
                         )}
@@ -66,17 +121,41 @@ function ProjectList() {
                             <p className="info">Currently no projects.</p>
                         )}
 
-                        {projects.map((project) => {
-                            return (
-                                <Project
-                                    title={project.title}
-                                    github={project.github}
-                                    live={project.live}
-                                    image={`${BACKEND}${project.image}`}
-                                    key={project.live}
-                                />
-                            );
-                        })}
+                        {!loading && search && searchProject.length < 1 && (
+                            <p className="info">No project for this search</p>
+                        )}
+
+                        {search && searchProject.length > 0 && (
+                            <>
+                                {searchProject.map((project) => {
+                                    return (
+                                        <Project
+                                            title={project.title}
+                                            github={project.github}
+                                            live={project.live}
+                                            image={`${BACKEND}${project.image}`}
+                                            key={project.live}
+                                        />
+                                    );
+                                })}
+                            </>
+                        )}
+
+                        {!search && (
+                            <>
+                                {projects.map((project) => {
+                                    return (
+                                        <Project
+                                            title={project.title}
+                                            github={project.github}
+                                            live={project.live}
+                                            image={`${BACKEND}${project.image}`}
+                                            key={project.live}
+                                        />
+                                    );
+                                })}
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
