f7b6c121320cf894ce55414a04647429d2462b38
Ritik <raj76941@gmail.com>
2024-12-03 16:09:01 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -266,7 +266,7 @@ export default function Home() {
         document.title = "Ritik Raj | Web Developer";
     }, []);
 
-    console.log("=================== HOME ROUTE 9 ====================");
+    console.log("=================== HOME ROUTE 0 ====================");
 
     return (
         <>
